b6b5a77157a109c77109e5bf1bade6c7b0f70250
Pepetox <pepetox@gmail.com>
2017-05-26 10:34:59 +0200

    prepare to share

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#gcloud beta functions deploy $1  --trigger-http --stage-bucket gs://staging.test-endpoints-uno.appspot.com
+#gcloud beta functions deploy $1  --trigger-http --stage-bucket $2
 
 
-gcloud beta functions deploy signup --stage-bucket cloud-pepetox-functions --trigger-http
-gcloud beta functions deploy login  --stage-bucket cloud-pepetox-functions --trigger-http
-gcloud beta functions deploy auth  --stage-bucket cloud-pepetox-functions --trigger-http
+gcloud beta functions deploy signup --stage-bucket your-bucket --trigger-http
+gcloud beta functions deploy login  --stage-bucket your-bucket --trigger-http
+gcloud beta functions deploy auth  --stage-bucket your-bucket --trigger-http
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Datastore = require('@google-cloud/datastore');
 const jwt = require('jwt-simple');
 const secret = "8emVuE72tN2JZ6C1ci0q";  //CHANGE FOR YOUR SECRET
-const passwordHash = require('password-hash');
+const passwordHash = require('password-hash'); //TODO: change for the new version
 const datastore = Datastore({namespace: "easylogin"});
+
+//need to have a body with email and password in an json
 exports.signup = (req, res) => {
   //review the body is correct
   if(JSON.stringify(req.body)==="{}"||(!req.body.email)||(!req.body.password)){
@@ -15,15 +17,19 @@ exports.signup = (req, res) => {
   if(typeof  password == 'number'){
     password = password.toString();
   }
+  //hash the password
   const hashedPassword = passwordHash.generate(password);
+  //make the object to insert datastore
   var miObjectData = {
     'email': req.body.email,
     'pw': hashedPassword
   }
+  //make the entity
   const entity = {
     key: miObjectKey,
     data: miObjectData
   };
+  //insert
   datastore.insert(entity)
   .then(() => {
     //res.status(200).send('Success: '+ JSON.stringify(entity));
@@ -39,6 +45,8 @@ exports.signup = (req, res) => {
     return
   });
 };
+//need to have a body with email and password in an json
+//returns a token
 exports.login = (req, res) => {
   if(JSON.stringify(req.body)==="{}"||(!req.body.email)||(!req.body.password)){
     res.status(400).json({"error":"No correct data send, please send email and password as aplication/json type body"});
@@ -75,7 +83,7 @@ var validateToken = (token) => {
   return decoded;
 
 }
-//example of validate function
+//example of validate function, need have token element in the body
 exports.auth = (req, res) => {
   if(JSON.stringify(req.body)==="{}"||(!req.body.token)){
     res.status(400).json({"error":"No correct data send, please send email and password as aplication/json type body"});
